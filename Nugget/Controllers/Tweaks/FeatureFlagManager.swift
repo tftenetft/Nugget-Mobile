4c4baf74f5d297136bdcd5680b8c369c849d0376
Tꓕ <15526289+tftenetft@users.noreply.github.com>
2024-10-22 21:48:58 +0200

    Update FeatureFlagManager.swift

--- Nugget/Controllers/Tweaks/FeatureFlagManager.swift
+++ Nugget/Controllers/Tweaks/FeatureFlagManager.swift
@@ -39,33 +39,33 @@ class FeatureFlagManager: ObservableObject {
                     flagList[flag] = ["DevelopmentPhase": value]
                 } else {
                     flagList[flag] = value
-                }plist["GenerativeModels"] = flagList}else
+                }plist["GenerativeModels"] = flagList;}else
                 if (EnabledFlag.category == FeatureFlagCategory.SpringBoard0) {
                     var value1 = true
                 if EnabledFlag.is_list {
                     flagList[flag] = ["Enabled": value1]
                 } else {
                     flagList[flag] = value1
-                } plist["GenerativeModels"] = flagList }else
+                } plist["GenerativeModels"] = flagList; }else
                 if (EnabledFlag.category == FeatureFlagCategory.Photos) {
                 if EnabledFlag.is_list {
                     flagList[flag] = ["DevelopmentPhase": value]
                 } else {
                     flagList[flag] = value
-                } plist[EnabledFlag.category.rawValue] = flagList }else
+                } plist[EnabledFlag.category.rawValue] = flagList; }else
                 if (EnabledFlag.category == FeatureFlagCategory.Photos0) {
                     var value1 = true
                 if EnabledFlag.is_list {
                     flagList[flag] = ["Enabled": value1]
                 } else {
                     flagList[flag] = value1
-                } plist[EnabledFlag.category.rawValue] = flagList }else {
+                } plist[EnabledFlag.category.rawValue] = flagList; }else {
                     var value1 = true
                    if EnabledFlag.is_list {
                     flagList[flag] = ["Enabled": value1]
                 } else {
                     flagList[flag] = value1
-                } plist[EnabledFlag.category.rawValue] = flagList
+                } plist[EnabledFlag.category.rawValue] = flagList;
                 }
             }
             //1
